ce512bf8b95d6a231d3d1ca141287fbff8efebda
Chen <pm83126492@gmail.com>
2021-10-24 23:28:54 +0800

    Add Chen

---
+++ GitTest/Assets/Chen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Chen Test");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
